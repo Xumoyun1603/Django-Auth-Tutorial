106c342dbad817a12574e5c8398c9a505deb3117
Xumoyun1603 <tuit20192022@gmail.com>
2022-03-26 18:11:30 +0500

    initial commit

---
+++ accounts/views.py
@@ -0,0 +1,10 @@
+from .forms import CustomUserCreationForm
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
+
+
+class SignUpView(CreateView):
+    form_class = CustomUserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'registration/signup.html'
+
